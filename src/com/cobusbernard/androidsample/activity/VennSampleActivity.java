d4f548848233be9c42b23737ddb82aa14c31560c
Cobus Bernard <cobus.bernard@gmail.com>
2012-02-23 16:52:10 +0200

    Playing with multi-circle layout

--- src/com/cobusbernard/androidsample/activity/VennSampleActivity.java
+++ src/com/cobusbernard/androidsample/activity/VennSampleActivity.java
@@ -19,6 +19,7 @@ public class VennSampleActivity extends Activity {
 	
 	private int circle1Value = 50;
 	private int circle2Value = 100;
+	private int circle3Value = 10;
 	
 	private Panel panel;
 	
@@ -36,6 +37,43 @@ public class VennSampleActivity extends Activity {
     }
     
     private void drawCircles() {
+    	int sidePad = 10;
+    	int smallCircleSpacing = 5;
+    	int scrollBarSpace = 60 + 10;
+    	
+    	Display display = getWindowManager().getDefaultDisplay(); 
+    	int width = display.getWidth();  // deprecated
+    	int height = display.getHeight();  // deprecated
+    	
+    	int maxRadius = ((int) Math.min(height, width)/ 2) - sidePad;
+    	int maxWidth = (int) Math.min(height, width);
+
+    	int x = width / 2;
+    	int y = maxRadius + sidePad;
+    	
+    	this.panel.getCircles().add(new CircleShape(x, y, maxRadius, Color.RED));
+    	
+    	//10-O-5-O-5-O-10
+    	//Pad(10)-Circle(smallR)-Space(5)-Circle(smallR)-Space(5)-Circle(smallR)-Pad(10)    	
+    	int smallR = (maxWidth - (2 * sidePad) - (3 * smallCircleSpacing)) / 8;
+    	
+    	int x1 = sidePad + smallR;
+    	int x2 = x1 + (2 * smallR) + smallCircleSpacing;
+    	int x3 = x2 + (2 * smallR) + smallCircleSpacing;
+    	int x4 = x3 + (2 * smallR) + smallCircleSpacing;
+    	
+    	int ySpaceLeft = height - ((2 * maxRadius) + (2 * sidePad) + scrollBarSpace);
+    	
+    	int y1 = (2 * maxRadius) + (2 * sidePad) + (ySpaceLeft / 2);
+
+    	this.panel.getCircles().add(new CircleShape(x1, y1, smallR, Color.BLUE));
+    	this.panel.getCircles().add(new CircleShape(x2, y1, smallR, Color.GREEN));
+    	this.panel.getCircles().add(new CircleShape(x3, y1, smallR, Color.YELLOW));
+    	this.panel.getCircles().add(new CircleShape(x4, y1, smallR, Color.GRAY));
+    	
+    }    
+    
+    private void drawCircles2() {
     	Display display = getWindowManager().getDefaultDisplay(); 
     	int width = display.getWidth();  // deprecated
     	int height = display.getHeight();  // deprecated
